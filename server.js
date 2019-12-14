0740329ed1e863f72dfe53fa471b4088d4cdaafd
tanatips <tanatip_s@hotmail.com>
2019-12-14 23:59:55 +0700

    Add watermark after upload

--- server.js
+++ server.js
@@ -2,8 +2,11 @@ const express = require("express");
 const cors = require('cors');
 const app = express();
 const fileupload = require("express-fileupload");
+const watermark = require("./libs/Watermark");
 // Set up a whitelist and check against it:
-var whitelist = ['http://localhost:9000/upload','http://localhost:3000'];
+var whitelist = [
+  'http://localhost:9000/upload','http://localhost:3000'
+, 'http://localhost:9000/watermark'];
 var corsOptions = {
   origin: function (origin, callback) {
     if (whitelist.indexOf(origin) !== -1) {
@@ -17,8 +20,8 @@ app.use(cors(corsOptions));
 app.use(fileupload());
 
 app.get('/',(req,res)=>{
-    //res.json({message:'API upload!'})
-    res.sendFile(__dirname+"/index.html");
+    res.json({message:'API upload!'})
+   
 });
 app.get('/general',(req,res)=>{
 
@@ -26,23 +29,34 @@ app.get('/general',(req,res)=>{
 app.get('/protected',(req,res)=>{
 
 });
-app.post('/upload',(req,res)=>{
+app.post('/upload',cors(corsOptions),(req,res)=>{
     if(req.files){
         console.log(req.files);
+        debugger;
         var file= req.files.filename;
             filename = file.name;
             file.mv("./upload/"+filename,function(err){
                 if(err){
                     console.log(err);
-                    //res.send("error occured");
                     res.json({message:err});
                 }
                 else{
                     res.json({message:"Done"});
                 }
-            })
+          });
     }
-})
+});
+app.get('/watermark',(req,res)=>{
+    var filename=req.query["filename"];
+    if(watermark.checkExtension(filename,"Photo"))
+    {
+        watermark.genWatermark(__dirname.concat("/upload/").concat(filename),
+        __dirname.concat("/images/logo/logo.jpg"),
+        __dirname.concat("/images/watermark/water_".concat(filename))
+        );
+    }
+
+});
 app.listen(9000,()=>{
     console.log('Application is running on port 9000')
 })
